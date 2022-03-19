b1cc92f031f817f8d4a3b5f4a03271b03fbb5779
Richard Laurent <ronaldorichard27@gmail.com>
2022-03-19 17:54:44 +0700

    LoopingComponent [pt. 10]

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <img alt="Vue logo" class="max-w-xs" src="./assets/logo.svg"/>
     <div class="mt-5">
       <h1>Hello World!</h1>
-      <HelloComponent msg="Richard Laurent" />
+      <hello-component msg="Richard Laurent" />
       <div :title="hoverHitung" class="the-Counter">The Counter : {{ hitung }}</div>
 
       <!-- Plus Buton -->
@@ -27,22 +27,57 @@
       <input v-model="pesan" class="inputVModel" />
     </div>
   </header>
+
+  <main class="container flex flex-col px-4 max-w-md md:max-w-xl mx-auto justify-center mt-10">
+
+    <div class="rounded-2xl shadow-md border border-slate-200 px-16 py-16">
+      <ol class="list-decimal">
+        <li v-for="(todo, i) in todos" :key="todo.id">
+          {{ todo.activity }} <span class="italic font-semibold">(index: {{i}})</span>
+        </li>
+      </ol>
+    </div>
+
+    <pengulangan-component 
+      v-for="i in pengulanganComponent" 
+      :key="i"
+      :pengulangan="i"
+    />
+
+    <div class="mt-96 mx-auto">--- End Content ---</div>
+  </main>
 </template>
 
 <script>
 import HelloComponent from './components/HelloComponent.vue'
+import PengulanganComponent from './components/PengulanganComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloComponent
+    HelloComponent,
+    PengulanganComponent
   },
   data() {
     return {
       hitung: 0,
       hoverHitung: "Hitung Angka",
       pesan: "\"\"",
-      status: false
+      status: false,
+      todos: [
+        {id: 1, activity: 'Requirements'},
+        {id: 2, activity: 'Design'},
+        {id: 3, activity: 'Development'},
+        {id: 4, activity: 'Testing'},
+        {id: 5, activity: 'Deployment'},
+        {id: 6, activity: 'Review'}
+      ],
+      pengulanganComponent: [
+        'Richard',
+        'Gio',
+        'John',
+        'Michael'
+      ]
     }
   },
   methods: {
@@ -66,7 +101,7 @@ export default {
     },
     resetBtn() {
       if (this.hitung != 0) {
-        this.status = true
+        this.status = !this.status
         this.hitung = 0
       } else if (this.hitung == 0) {
         this.status = false
---
+++ src/components/PengulanganComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{pengulangan}}
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        pengulangan: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
